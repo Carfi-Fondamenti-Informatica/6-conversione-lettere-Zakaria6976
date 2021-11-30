a0ace80c8dc4ba44b438f52dda4c70c1db881a3c
Zakaria6976 <91836213+Zakaria6976@users.noreply.github.com>
2021-11-30 12:42:45 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -3,7 +3,7 @@ bool conversione (char &a) {
   bool t=true;
   if (a>=65 and a<=90 or a>=97 and a<=122){
   t=true;
-    if(a >=65 and a<>90{
+    if(a >=65 and a<=90{
       a =a + 32;
       returna;
     }else{
